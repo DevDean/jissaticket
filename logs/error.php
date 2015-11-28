40ae5f6872706ed7af5f18d9c99db9e822310806
Dev Dean <adriandean12@gmail.com>
2015-11-28 18:53:46 +0800

    Added catgories on home page instead of articles. Suggestions for the site gui will be welcomed

--- logs/error.php
+++ logs/error.php
@@ -5,3 +5,5 @@
 
 #Fields: datetime	priority clientip	category	message
 2015-11-02T13:05:37+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-11-28T02:14:08+00:00	INFO 192.168.8.102	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-11-28T02:14:13+00:00	INFO 192.168.8.102	joomlafailure	Username and password do not match or you do not have an account yet.
